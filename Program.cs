6f0584f80926901ea8dd613c03a281802c94cf28
guilhermecunha777 <guihcunha12@gmail.com>
2025-06-04 14:16:06 -0300

    pronto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Prgram
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "111111111111111", 32);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("whatsApp");
+
+        Smartphone iphone = new Iphone("987654321", "iPhone 13", "222222222222222", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("instagram");
+    }
+}
